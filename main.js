35e8037e0a288242d5d17e703a5768411a44241d
Codeuzz <belinda.zilevu@gmail.com>
2024-10-03 12:30:35 +0200

    finished scatterplot, need to add tooltip

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 import './style.css'
 import * as d3 from "d3";
 
-document.getElementById('app').innerHTML += `<div id="container"></div>`
+document.getElementById('app').innerHTML += `
+<div id="container"></div>
+<div id='title-text'>
+  <h1>Doping in Professional Bicycle Racing</h1>
+  <p>35 Fastest times up Alpe d'Huez</p>
+</div>
+
+`
 
 fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json')
 .then(res => {
@@ -13,6 +20,7 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
 })
 .then(data => {
   makeSvg();
+  makeAxis(data)
   makeCircles(data);
   console.log(data)
 })
@@ -20,29 +28,81 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
   console.error('Error fetching the dataa', error);
 })
 
-const height = 500;
-const width = 1000;
-const padding = 40;
+const height = 800;
+const width = 1200;
+const padding = 50;
+let scaleX
+let scaleY
+
 
 const makeSvg = () => {
   d3.select('#container')
   .append('svg')
   .attr('height', height)
-  .attr('width', width)
-  .style('border', '1px solid red')
-  .attr('x', 100)
+  .attr('width', width )
+  // .style('border', '1px solid red')
 }
 
 
 const makeCircles = data => {
   const svg =  d3.select('svg')
+
   svg.selectAll('circle')
   .data(data)
   .enter()
   .append('circle')
-  .attr('r', 7)
-  .attr('cx', (d, i) => 7 * i)
-  .attr('cy', (d, i) => height - parseFloat(d.Time) * 10 )
-  .attr('fill', 'white')
+  .attr('r', 9)
+  .attr('cx', d => scaleX(d.Year))
+  .attr('cy', (d, i) => scaleY(timeToSeconds(d.Time)) )
+  .attr('fill', d => d.Doping == "" ? '#18d100' : 'red')
+  .attr('stroke', 'black')
+  .attr('stroke-width', '2px')
 
+
+}
+
+const timeToSeconds = timeStr => {
+  const [minutes, seconds] = timeStr.split(":").map(Number)
+  return minutes * 60 + seconds
+}
+
+const secondsToTime = totalSeconds => {
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60
+  return `${minutes}:${seconds.toString().padStart(2, '0')}`
+}
+
+const makeAxis = data => {
+  const svg =  d3.select('svg')
+  const maxYear = d3.max(data.map(d => d.Year))
+  const minYear = d3.min(data.map(d => d.Year)) 
+
+
+  scaleX = d3.scaleLinear()
+    .domain([minYear - 1, maxYear + 1])  
+    .range([padding, width - padding]);
+
+  const axisX = d3.axisBottom(scaleX)
+    .tickFormat(d3.format('d'))
+
+  svg.append('g')
+    .attr("transform", `translate(0, ${height - padding})`)
+    .attr('id', 'x-axis')
+    .call(axisX)
+
+
+  const TimeInSeconds = data.map(d => timeToSeconds(d.Time))
+
+  scaleY = d3.scaleLinear()
+    .domain([d3.max(TimeInSeconds) + 10, d3.min(TimeInSeconds)+ 20])
+    .range([height - padding, padding])
+
+  const axisY = d3.axisLeft(scaleY)
+  .tickFormat(secondsToTime)
+
+
+  svg.append('g')
+  .attr("transform", `translate(${padding}, 0)`)
+  .attr('id', 'y-axis')
+  .call(axisY)
 }
